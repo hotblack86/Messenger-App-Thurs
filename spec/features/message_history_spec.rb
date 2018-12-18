dfd391348c313d5eda09bf5fb4f10c03c7e5c68c
Hui An Yang <hecatie36@hotmail.com>
2018-12-18 14:06:29 +0000

    minor refactoring before implementing class

--- spec/features/message_history_spec.rb
+++ spec/features/message_history_spec.rb
@@ -6,8 +6,8 @@ feature "Message history" do
     click_button('Send')
     fill_in('message', :with => 'test2')
     click_button('Send')
-    expect(page).to have_content('test1')
     expect(page).to have_content('test2')
-    expect(page).to have_content(Time.now)
+    expect(page).to have_content('test1')
+    expect(page).to have_content(Time.now.strftime("%d-%m-%Y %H:%M"))
   end
 end
